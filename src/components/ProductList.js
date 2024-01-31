16aafb7d3ff121b615c8fbf544f6c98e22b9ac02
Mayada Elsayed <emayada44@gmail.com>
2024-01-31 10:19:15 +0200

    Your commit message

---
+++ src/components/ProductList.js
@@ -0,0 +1,20 @@
+// components/ProductList.js
+import React from "react";
+import products from "../data/products";
+
+const ProductList = () => {
+    return (
+        <div>
+            <h2>Product List</h2>
+            <ul>
+                {products.map((product) => (
+                    <li key={product.id}>
+                        {product.name} - ${product.price}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default ProductList;
---
+++ src/components/ShoppingCart.js
@@ -0,0 +1,13 @@
+// components/ShoppingCart.js
+import React from "react";
+
+const ShoppingCart = () => {
+    return (
+        <div>
+            <h2>Shopping Cart</h2>
+            {/* Display cart items */}
+        </div>
+    );
+};
+
+export default ShoppingCart;
